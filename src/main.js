b81e24b965503cc6abd4afb6802ae6fd682b6157
Mohit Bhagat <m5bhagat@edu.uwaterloo.ca>
2020-05-03 15:45:00 -0400

    add player sprite and basic canvas setup

--- src/main.js
+++ src/main.js
@@ -1,1 +1,43 @@
 // colorbound
+
+var canvas = null;
+var ctx = null;
+
+var playerImage = new Image();
+var playerReady = false;
+
+playerImage.onload = function() {
+    playerReady = true;
+}
+
+playerImage.src = "graphics/player1.png";
+
+function init() {
+	canvas = document.createElement("canvas");
+	document.body.appendChild(canvas);
+
+	canvas.width = 960;
+	canvas.height = 720;
+
+	canvas.style["position"] = "fixed";
+	canvas.style["top"] = "50%";
+	canvas.style["left"] = "50%";
+	canvas.style["transform"] = "translate(-50%, -50%)";
+	canvas.style["border"] = "solid";
+
+	ctx = canvas.getContext("2d");
+}
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	ctx.fillStyle = "#00001D";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	if(playerReady) {
+		ctx.drawImage(playerImage, 100, 100);
+	}
+}
+
+init();
+draw();
