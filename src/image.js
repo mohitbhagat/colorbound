956c3d81470bf72823252c7afab120fb117c5602
Mohit Bhagat <m5bhagat@edu.uwaterloo.ca>
2020-07-11 14:00:00 -0400

    fix title screen crash and image loading

--- src/image.js
+++ src/image.js
@@ -36,6 +36,24 @@ const EXPLOSION_FRAME_TIME = 1 / 30;
 const EXPLOSION_FRAME_WIDTH = 120;
 const EXPLOSION_FRAME_HEIGHT = 94;
 
+var titleReady = false;
+var titleImage = new Image();
+
+titleImage.onload = function() {
+    titleReady = true;
+}
+
+titleImage.src = "graphics/title.png";
+
+var heartReady = false;
+var heartImage = new Image();
+
+heartImage.onload = function() {
+    heartReady = true;
+}
+
+heartImage.src = "graphics/heart.png";
+
 var rocketReady = false;
 var rocketImage = new Image();
 
@@ -128,14 +146,6 @@ yellowEnemyImage.onload = function() {
 
 yellowEnemyImage.src = "graphics/yellowenemy.png";
 
-var heartReady = false;
-var heartImage = new Image();
-
-heartImage.onload = function() {
-    heartReady = true;
-}
-
-heartImage.src = "graphics/heart.png";
 var ammoReady = false;
 var ammoImage = new Image();
 
